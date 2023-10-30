91e2550669031002b72422bfaef79b851043957c
mediainformationofficer@gmail.com <mediainformationofficer@gmail.com>
2023-10-30 10:28:51 -0500

    --age means "first print then decrease the value" while age-- means "first decrease the value then print"

--- master.java
+++ master.java
@@ -1,13 +1,13 @@
 class HelloWorld {
   public static void main(String[] args) {
     
-    /*++age means "first print then increase the value" while age++ means "first increase the value then print"*/
+    /*--age means "first print then decrease the value" while age-- means "first decrease the value then print"*/
     int age = 11;
-    System.out.println(++age);
-    System.out.println(age++);
-    System.out.println(++age);
-    System.out.println(age++);
-    System.out.println(++age);
+    System.out.println(--age);
+    System.out.println(age--);
+    System.out.println(age);
+    System.out.println(--age);
+    System.out.println(age);
     
   }// end method
 }// end class
