30aaf7799488b3b643e47bf221c83c876afa0867
mediainformationofficer@gmail.com <mediainformationofficer@gmail.com>
2023-10-30 10:22:18 -0500

    ++age means "first print then increase the value" while age++ means "first increase the value then print"

--- master.java
+++ master.java
@@ -1,5 +1,13 @@
 class HelloWorld {
   public static void main(String[] args) {
     
+    /* ++age means "first print then increase the value" while age++ means "first increase the value then print" */
+    int age = 11;
+    System.out.println(++age);
+    System.out.println(age++);
+    System.out.println(++age);
+    System.out.println(age++);
+    System.out.println(++age);
+    
   }// end method
 }// end class
