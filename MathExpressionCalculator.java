ab41d3b1121c443b70c7789c8a60423ea57dd7e2
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-01 10:05:59 -0500

    computes a specified mathematical expression as input from the user and prints the result

---
+++ MathExpressionCalculator.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+/* computes a specified mathematical expression as input from the user and prints the result */
+public class MathExpressionCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a mathematical expression: ");
+        String input = scanner.nextLine();
+
+        try {
+            double result = evaluateExpression(input);
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static double evaluateExpression(String expression) {
+        try {
+            // Replace any spaces to ensure a clean expression
+            expression = expression.replaceAll("\\s", "");
+
+            // Use Java's built-in arithmetic evaluation
+            double result = eval(expression);
+            return result;
+        } catch (Exception e) {
+            throw new ArithmeticException("Invalid expression");
+        }
+    }
+
+    public static double eval(String expression) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < expression.length()) ? expression.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < expression.length()) {
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                }
+                return x;
+            }
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (; ; ) {
+                    if (eat('+')) x += parseTerm();
+                    else if (eat('-')) x -= parseTerm();
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (; ; ) {
+                    if (eat('*')) x *= parseFactor();
+                    else if (eat('/')) x /= parseFactor();
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor();
+                if (eat('-')) return -parseFactor();
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) {
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') {
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(expression.substring(startPos, this.pos));
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                }
+
+                return x;
+            }
+        }.parse();
+    }
+}
