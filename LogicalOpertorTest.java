35e89ec2b8e1a0c92b0dabc04ad9c6e78596fb26
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-01 10:56:19 -0500

    test bitwise logical operators

---
+++ LogicalOpertorTest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/* test bitwise logical operators */
+class LogicalOpertorTest {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 3;
+    int c = a << b;
+    System.out.println(c)
+  }// end method
+}// end class
+
+//10 - 0101 0000
+// output is 80
