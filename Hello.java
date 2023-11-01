1d3a8795f23a582119b2cf604a61803644830daf
mediainformationofficer@gmail.com <mediainformationofficer@gmail.com>
2023-10-31 21:26:43 -0500

    print Hello and my name on a separate line

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package com.test;
+
+//print Hello and my name on a separate line
+public class Hello {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Antonio");
+    } // end method
+    
+}// end class
