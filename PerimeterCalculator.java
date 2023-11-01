6f2391d17307293761b8859714b533f8c03b49e2
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-01 10:20:51 -0500

    takes input as a double and prints the area and perimeter of a circle

---
+++ PerimeterCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/* takes input as a double and prints the area and perimeter of a circle */
+public class PerimeterCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Perimeter is = " + perimeter);
+        System.out.println("Area is = " + area);
+    }
+}
