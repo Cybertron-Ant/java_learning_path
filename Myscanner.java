9e2a55a40ee92af796d8c436edecc217cd51f4ef
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-01 11:33:51 -0500

    reformatted 'Myscanner' class

--- Myscanner.java
+++ Myscanner.java
@@ -7,12 +7,16 @@ public class MyScanner {
   public static void main(String[] args) {
     // create instance of 'Scanner' class
     Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Enter 1st Number: ");
     // nextInt() method of Scanner class reads in a string of digits (characters) and converts them into an int type
     int a = scan.nextInt();
+    
+    System.out.println("Enter 2nd Number: ");
     int b = scan.nextInt();
     int c = a + b;
 
-    System.out.println(c);
+    System.out.println("Result: " + c);
     
   }// end method
 }// end class
