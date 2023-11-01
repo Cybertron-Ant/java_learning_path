a5fdec8c17ecff0df9653d428d4f1586a8e921c5
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-01 11:25:14 -0500

    create instance of 'Scanner' class

--- Myscanner.java
+++ Myscanner.java
@@ -5,6 +5,8 @@ import java.util.Scanner
 
 public class MyScanner {
   public static void main(String[] args) {
+    // create instance of 'Scanner' class
+    Scanner scan = new Scanner(System.in);
     
   }// end method
 }// end class
