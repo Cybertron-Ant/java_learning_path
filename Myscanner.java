3978ba254520a0584f7812def46dd9c540b2225b
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-01 11:28:00 -0500

    nextInt() method of Scanner class reads in a string of digits (characters) and converts them into an int type

--- Myscanner.java
+++ Myscanner.java
@@ -7,6 +7,12 @@ public class MyScanner {
   public static void main(String[] args) {
     // create instance of 'Scanner' class
     Scanner scan = new Scanner(System.in);
+    // nextInt() method of Scanner class reads in a string of digits (characters) and converts them into an int type
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    int c = a + b;
+
+    System.out.println(c);
     
   }// end method
 }// end class
