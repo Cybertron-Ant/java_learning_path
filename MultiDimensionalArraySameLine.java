cf9c79b65f3419ac6d8ca9d75f7505b6c40b6e01
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 14:06:37 -0500

    declared and defined multi-dimensional array with values

--- MultiDimensionalArraySameLine.java
+++ MultiDimensionalArraySameLine.java
@@ -1,5 +1,13 @@
 class MultiDimensionalArraySameLine {
   public static void main(String[] args) {
+
+    // declared and defined multi-dimensional array with values
+    int[][] multiDimArray = {
+            {10, 20, 100},
+            {5000, 50000, 1000},
+            {207, 108, 108},
+            {5002, 50008}
+    };// end multiDimArray
     
   }// end method
 }// end class
