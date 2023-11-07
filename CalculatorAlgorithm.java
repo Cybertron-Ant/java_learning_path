f9f47fa31645c3248baeae9ff4919167b5572a7f
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 08:30:34 -0500

    basic structure of the CalculatorAlgorithm class

---
+++ CalculatorAlgorithm.java
@@ -0,0 +1,14 @@
+// import class to take user input
+import java.util.Scanner;
+
+
+/* basic structure of the CalculatorAlgorithm class */
+class CalculatorAlgorithm {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // main methods code goes here
+      
+    }// end main
+
+}// end class
