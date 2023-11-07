3768cb97a2133be6f473a73adc23dc182d789276
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 13:46:39 -0500

    define the array with the specified number of elements based on user input value

--- DefineArrayValuesLater.java
+++ DefineArrayValuesLater.java
@@ -7,6 +7,9 @@ class DefineArrayValuesLater {
     // prompt user to enter the number of elements
     System.out.print("Enter the number of elements: ");
     int numberOfElements = scanner.nextInt();
+
+    // define the array with the specified number of elements based on user input value
+    myArray = new int[numberOfElements];
     
   }// end method
 }// end class
