0f53e8ea57cda18f9679ed45e2c4bc6ab17f5e8e
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 10:49:26 -0500

    define 2D integer array named 'a' with 4 rows and 2 columns, and each element of the array holds an integer value

--- MultiDimensionalArray.java
+++ MultiDimensionalArray.java
@@ -1,5 +1,13 @@
 class MultiDimensionalArray {
   public static void main(String[] args) {
+    /* define 2D integer array named 'a' with 4 rows and 2 columns, and each element of the array holds an integer value.
+    The outer array[4] has 4 elements, the rows, each inner array[2] has 2 elements, the columns.
+    10  20
+    20  30
+    30  10
+    200 10
+    access individual elements in the array using indices, for eg, a[0][0] would be 10, a[1][1] would be 30 */
+    int[4][2] a = { {10, 20}, {20, 30}, {30, 10}, {200, 10} }; // 8
     
   }// end method
 }// end class
