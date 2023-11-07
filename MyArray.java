e7249993658f48df1791ae0d03a4f6064123ee76
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 10:05:55 -0500

    Update MyArray.java class name

--- MyArray.java
+++ MyArray.java
@@ -1,4 +1,4 @@
-class HelloWorl {
+class MyArray {
   public static void main(String[] args) {
     /* [] is used to initialize an array, [5] sets the lenght of the array to 5 - 20 bytes */
     int[] a = new int[5];
