958a425bb1ffbd1e37c04ea2814c980573fcd57c
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 11:36:16 -0500

    declare 2D integer array named 'a' with 5 rows and 2 columns

--- MultiDimensionalArrayTwo.java
+++ MultiDimensionalArrayTwo.java
@@ -1,5 +1,14 @@
 class MultiDimensionalArrayTwo {
   public static void main(String[] args) {
+
+    /* declare 2D integer array named 'a' with 5 rows and 2 columns, and each element of the array holds an integer value.
+    The outer array[4] has 4 elements, the rows, each inner array[2] has 2 elements, the columns.
+    10  20
+    20  30
+    30  10
+    200 10
+    access individual elements in the array using indices, for eg, a[0][0] would be 10, a[1][1] would be 30 */
+    int[][] a = new int[5][2]; // 8
     
   }// end method
 }// end class
