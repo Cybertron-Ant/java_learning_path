0cb2074e74b7ce0aceeb2e899bb688dd24ef5c7e
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 13:29:45 -0500

    print all even numbers from array using a 'for' loop

--- SingleDimensionalArrayTwo.java
+++ SingleDimensionalArrayTwo.java
@@ -3,6 +3,14 @@ class SingleDimensionalArrayTwo {
 
     // declare and define an array containing values in same line
     int[] myArray = {10, 20, 100, 5000, 50000, 1000, 207, 108, 108, 5002, 50008};
+
+
+    // print all even numbers from array using a 'for' loop
+    for (int num : myArray) {
+      if (num % 2 == 0) {
+        System.out.println(num);
+      }// end IF
+    }// end FOR
     
   }// end method
 }// end class
