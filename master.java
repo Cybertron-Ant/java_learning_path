b0632ae89ed46f99a1ac117021475b0de7148b55
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 09:58:22 -0500

    [] is used to initialize an array, [5] sets the lenght of the array to 5 - 20 bytes

--- master.java
+++ master.java
@@ -1,5 +1,7 @@
 class HelloWorld {
   public static void main(String[] args) {
+    /* [] is used to initialize an array, [5] sets the lenght of the array to 5 - 20 bytes */
+    int[] a = new int[5];
     
   }// end method
 }// end class
