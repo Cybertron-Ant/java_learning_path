cf8de2e08edfd4707590099dcf3db29abbf07c0f
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 13:45:17 -0500

    prompt user to enter the number of elements

--- DefineArrayValuesLater.java
+++ DefineArrayValuesLater.java
@@ -3,6 +3,10 @@ class DefineArrayValuesLater {
 
     // declare an array
     int[] myArray;
+
+    // prompt user to enter the number of elements
+    System.out.print("Enter the number of elements: ");
+    int numberOfElements = scanner.nextInt();
     
   }// end method
 }// end class
