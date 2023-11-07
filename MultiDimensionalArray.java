71658fa7197e15d7ce4d5151e65c5edfb40c2660
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 10:58:38 -0500

    reassign the value at the second row(index 1) and the first column(index 0) to 2008

--- MultiDimensionalArray.java
+++ MultiDimensionalArray.java
@@ -8,6 +8,13 @@ class MultiDimensionalArray {
     200 10
     access individual elements in the array using indices, for eg, a[0][0] would be 10, a[1][1] would be 30 */
     int[4][2] a = { {10, 20}, {20, 30}, {30, 10}, {200, 10} }; // 8
+
+    /* reassign the value at the second row(index 1) and the first column(index 0) to 2008
+        10  20
+        2008  30
+        30  10
+        200 10*/
+    a[1][0] = 2008;
     
   }// end method
 }// end class
