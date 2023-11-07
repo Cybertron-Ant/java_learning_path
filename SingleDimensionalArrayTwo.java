b1525d789d62fadcee7891ea591d026244cfb7fb
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 13:26:41 -0500

    declare and define an array containing values in same line

---
+++ SingleDimensionalArrayTwo.java
@@ -0,0 +1,8 @@
+class SingleDimensionalArrayTwo {
+  public static void main(String[] args) {
+
+    // declare and define an array containing values in same line
+    int[] myArray = {10, 20, 100, 5000, 50000, 1000, 207, 108, 108, 5002, 50008};
+    
+  }// end method
+}// end class
