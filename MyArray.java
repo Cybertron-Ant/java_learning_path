5f3fa27622755c2bb92ec8be7578857884448f07
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 10:00:41 -0500

    a[0] sets the first index position of the array to the value 10

--- MyArray.java
+++ MyArray.java
@@ -1,7 +1,10 @@
-class HelloWorld {
+class HelloWorl {
   public static void main(String[] args) {
     /* [] is used to initialize an array, [5] sets the lenght of the array to 5 - 20 bytes */
     int[] a = new int[5];
+
+    /* a[0] sets the first index position of the array to the value 10 */
+    a[0] = 10;
     
   }// end method
 }// end class
