d04ad969147866e0320c292e2a747c7b56c49511
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 14:10:33 -0500

    question #3, changed variable names

--- MultiDimensionalArraySameLine.java
+++ MultiDimensionalArraySameLine.java
@@ -2,23 +2,24 @@ class MultiDimensionalArraySameLine {
   public static void main(String[] args) {
 
     // declared and defined multi-dimensional array with values
-    int[][] multiDimArray = {
+    int[][] MyMultiDimArray = {
             {10, 20, 100},
             {5000, 50000, 1000},
             {207, 108, 108},
             {5002, 50008}
-    };// end multiDimArray
+    };// end MyMultiDimArray
 
 
     // print even numbers from array using a 'for' loop
         System.out.println("Even numbers in the array:");
-    for (int i = 0; i < multiDimArray.length; i++) {
-      for (int j = 0; j < multiDimArray[i].length; j++) {
-        if (multiDimArray[i][j] % 2 == 0) {
-          System.out.println(multiDimArray[i][j]);
+    for (int i = 0; i < MyMultiDimArray.length; i++) {
+      for (int j = 0; j < MyMultiDimArray[i].length; j++) {
+        if (MyMultiDimArray[i][j] % 2 == 0) {
+          System.out.println(MyMultiDimArray[i][j]);
         }// end IF
       }// end FOR
     }// end FOR
     
   }// end method
 }// end class
+// question #3
