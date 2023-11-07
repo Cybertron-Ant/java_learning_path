6ea3429cf2ec0989638aa235c4f58d677e5b50d7
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-07 14:18:33 -0500

    create instance of 'Scanner" object/class

--- MultiDimensionalArrayValuesLater.java
+++ MultiDimensionalArrayValuesLater.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 class MultiDimensionalArrayValuesLater {
   public static void main(String[] args) {
+
+    /* create instance of 'Scanner" object/class */
+    Scanner scanner = new Scanner(System.in);
     
   }// end method
 }// end class
