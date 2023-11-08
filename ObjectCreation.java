92b348c5b803710072692507f01a4f5e8a8745ff
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-08 11:10:56 -0500

    create two instances of the 'A' class named 'a1' and 'a2'

--- ObjectCreation.java
+++ ObjectCreation.java
@@ -11,6 +11,10 @@ class ObjectCreation {
       /* change the value of 'number' to 1000 and then print it again  */
       number = 1000;
       System.out.println(number);
+
+      /* create two instances of the 'A' class named 'a1' and 'a2' */
+      A a1 = new A();
+      A a2 = new A();
       
     }// end 'main'
 }// end class ObjectCreation
