19b95f2371a3d1781a26bca7144e4ad093b0b7c2
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-08 11:04:51 -0500

    define the 'main' method

--- ObjectCreation.java
+++ ObjectCreation.java
@@ -1,4 +1,7 @@
 /* define the 'ObjectCreation' class */
 class ObjectCreation {
-
+    /* define the 'main' method */
+    public static void main(String[] args) {
+      
+    }// end 'main'
 }// end class ObjectCreation
