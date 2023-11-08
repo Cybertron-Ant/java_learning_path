c4cd703c1e57a1cacf03b676dd10b4968b7c4344
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-08 11:05:44 -0500

    declare an integer variable named 'number' and assign it the value 10000

--- ObjectCreation.java
+++ ObjectCreation.java
@@ -2,6 +2,8 @@
 class ObjectCreation {
     /* define the 'main' method */
     public static void main(String[] args) {
+      /* declare an integer variable named 'number' and assign it the value 10000 */
+      int number = 10000;
       
     }// end 'main'
 }// end class ObjectCreation
