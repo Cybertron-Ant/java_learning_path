bf548a20eebb0fcd6acece058bee1fb28dd4c3bb
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-08 10:44:24 -0500

    define the 'main' method within the 'ClassProperty' class, which is the entry point for the Java program

--- ClassProperty.java
+++ ClassProperty.java
@@ -1,2 +1,4 @@
 /* create a new class called 'class ClassProperty' */
 class ClassProperty {
+  /* define the 'main' method within the 'ClassProperty' class, which is the entry point for the Java program */
+      public static void main(String[] args) {
