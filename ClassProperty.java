681c263becbf09bdb4f5c9b04d7e344d459fec6f
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-08 10:45:23 -0500

    declare an integer variable number and assign it the value 10000

--- ClassProperty.java
+++ ClassProperty.java
@@ -2,3 +2,5 @@
 class ClassProperty {
   /* define the 'main' method within the 'ClassProperty' class, which is the entry point for the Java program */
       public static void main(String[] args) {
+        /* declare an integer variable number and assign it the value 10000 */
+        int number = 10000;
