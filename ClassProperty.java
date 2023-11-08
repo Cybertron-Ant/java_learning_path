fe82ac38723191389eef8b5fb76c63df3273f14c
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-08 10:46:17 -0500

    print the value of the 'number' variable

--- ClassProperty.java
+++ ClassProperty.java
@@ -4,3 +4,6 @@ class ClassProperty {
       public static void main(String[] args) {
         /* declare an integer variable number and assign it the value 10000 */
         int number = 10000;
+
+        /* print the value of the 'number' variable */
+        System.out.println(number);
