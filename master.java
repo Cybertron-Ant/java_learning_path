6a967b6d9ac2b37e19462d9d69c14e0193c2e69f
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-09 13:47:21 -0500

    outside 'main', assign 'a' to 5

--- master.java
+++ master.java
@@ -1,6 +1,9 @@
 class HelloWorld {
   /* outside 'main' declare private variable 'a' */
   private static int a;
+  
+  /* outside 'main', assign 'a' to 5 */
+  a = 5;
   public static void main(String[] args) {
     
   }// end method
