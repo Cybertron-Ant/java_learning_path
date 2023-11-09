45224a299b9b4bbb5e3d09ea44a3a5e08dd8b63d
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-09 13:50:23 -0500

    if i print the value of 'a', an error will occur, because i can't declare then reassign a variable outside the 'main' method
    if i print the value of 'a', an error will occur, because i can't declare then reassign a variable outside the 'main' method

--- master.java
+++ master.java
@@ -5,6 +5,9 @@ class HelloWorld {
   /* outside 'main', assign 'a' to 5 */
   a = 5;
   public static void main(String[] args) {
+
+    /* if i print the value of 'a', an error will occur, because i can't declare then reassign a variable outside the 'main' method */
+    System.out.println(a);
     
   }// end method
 }// end class
