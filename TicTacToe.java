2c8504a99c06c8e322198747e437a9dc202710e9
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-10 13:57:49 -0500

    instantiate Scanner as 'input' object to get user input

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,5 +4,8 @@ import java.util.Scanner;
 class TicTacToe {
   public static void main(String[] args) {
 
+    // instantiate Scanner as 'input' object to get user input
+    Scanner input = new Scanner(System.in);
+
   }// end method
 }// end class
