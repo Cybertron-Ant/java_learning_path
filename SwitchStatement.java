27e007c420abe1cce0ebeb2421e75a0bd2e98aef
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-14 11:27:15 -0500

    declare a 'switch' statement using the value of 'marks'

--- SwitchStatement.java
+++ SwitchStatement.java
@@ -4,5 +4,10 @@ class SwitchStatement {
       /* initialize a variable 'marks' with a value of 100, within the 'main' method */
       int marks = 100;
 
+      /* declare a 'switch' statement using the value of 'marks' */
+      switch(marks){
+              
+      }// end SWITCH
+
     }// end method 'main'
 }// end class 'SwitchStatement'
