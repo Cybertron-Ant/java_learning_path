78325f828edc195488e5d2846c51e8fdf391fcbd
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-14 11:06:58 -0500

    an 'if-else' statement to determine the scholarship based on the marks obtained, the code to check the value of 'marks'

--- NestedIfElse.java
+++ NestedIfElse.java
@@ -3,6 +3,17 @@ class NestedIfElse {
 
       /* declare an integer variable 'marks' and assign it a value representing a student's marks, within the 'main' method */
       int marks = 99; // Change this value to test different scenarios
+
+      /* an 'if-else' statement to determine the scholarship based on the marks obtained, the code to check the value of 'marks' */
+      if (marks == 100) {
+        System.out.println("100% Scholarship");
+      } else if (marks > 80) {
+        System.out.println("75% Scholarship");
+      } else if (marks > 50) {
+        System.out.println("50% Scholarship");
+      } else {
+        System.out.println("No Scholarship");
+      }// end ELSE
     
     }// end method 'main'
 }// end class NestedIfElse
