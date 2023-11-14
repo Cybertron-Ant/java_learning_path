e1a1b3c1df0bcb12d95c9ed39deaa4385bc0a032
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-14 11:29:12 -0500

    check if 'marks' variable is 100 and executes the code block if true

--- SwitchStatement.java
+++ SwitchStatement.java
@@ -6,6 +6,11 @@ class SwitchStatement {
 
       /* declare a 'switch' statement using the value of 'marks' */
       switch(marks){
+
+        /* check if 'marks' variable is 100 and executes the code block if true */
+        case 100 :
+          System.out.println("Input is 100.");
+          break;
               
       }// end SWITCH
 
