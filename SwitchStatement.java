3d64ddd879c8cc2506316e2e8083cc082a67e13d
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-14 11:25:23 -0500

    initialize a variable 'marks' with a value of 100, within the 'main' method

--- SwitchStatement.java
+++ SwitchStatement.java
@@ -1,5 +1,8 @@
 class SwitchStatement {
     public static void main(String[] args) {
 
+      /* initialize a variable 'marks' with a value of 100, within the 'main' method */
+      int marks = 100;
+
     }// end method 'main'
 }// end class 'SwitchStatement'
