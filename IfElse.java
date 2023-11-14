048fbc5dd94aaaed8e8d7f9871ac0f6fa966df8f
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-14 10:47:02 -0500

    declare an integer variable a and assign it a value, within the 'main' method

--- IfElse.java
+++ IfElse.java
@@ -1,5 +1,8 @@
 class IfElse {
   public static void main(String[] args) {
 
+    /* declare an integer variable a and assign it a value, within the 'main' method */
+    int a = -10; // You can change this value to test different numbers
+
   }// end method 'main'
 }// end class IfElse
