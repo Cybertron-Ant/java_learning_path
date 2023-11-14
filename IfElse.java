4ea23a0085ed426443f76e0a40eaee429c5629c8
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-14 10:49:45 -0500

    an 'if-else' statement to check if a number is positive or negative, the code to check the value of 'a'

--- IfElse.java
+++ IfElse.java
@@ -4,5 +4,14 @@ class IfElse {
     /* declare an integer variable a and assign it a value, within the 'main' method */
     int a = -10; // You can change this value to test different numbers
 
+    /* an 'if-else' statement to check if a number is positive or negative, the code to check the value of 'a' */
+    if (a > 0) {
+      System.out.println("Number is positive.");
+    } else if (a < 0) {
+      System.out.println("Number is negative.");
+    } else {
+      System.out.println("Number is zero.");
+    }// end ELSE
+
   }// end method 'main'
 }// end class IfElse
