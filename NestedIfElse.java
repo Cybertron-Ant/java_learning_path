29393a9a33f0f5dc85a8ad5a58453dbf0bd18395
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-14 11:03:59 -0500

    declare an integer variable 'marks' and assign it a value representing a student's marks, within the 'main' method

--- NestedIfElse.java
+++ NestedIfElse.java
@@ -1,5 +1,8 @@
 class NestedIfElse {
     public static void main(String[] args) {
+
+      /* declare an integer variable 'marks' and assign it a value representing a student's marks, within the 'main' method */
+      int marks = 99; // Change this value to test different scenarios
     
     }// end method 'main'
 }// end class NestedIfElse
