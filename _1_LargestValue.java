10cc3097d042c6a021ae05c8db5c4d638df07030
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-20 14:16:53 -0800

    select the first element from the array and consider it as the initial "chosen one"/entry point

--- _1_LargestValue.java
+++ _1_LargestValue.java
@@ -4,5 +4,8 @@ class _1_LargestValue {
     // create an array filled with various numbers
     int[] universeOfNumbers = {33, 81, 107, 100, 47, 59, 58};
 
+    // select the first element from the array and consider it as the initial "chosen one"/entry point
+    int chosenOne = universeOfNumbers[0];
+
   }// end method
 }// end class
