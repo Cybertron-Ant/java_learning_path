dff29dd4a9f9209233d1e399c01537297b317f53
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-20 14:19:23 -0800

    travel through the array to find the largest element

--- _1_LargestValue.java
+++ _1_LargestValue.java
@@ -7,5 +7,14 @@ class _1_LargestValue {
     // select the first element from the array and consider it as the initial "chosen one"/entry point
     int chosenOne = universeOfNumbers[0];
 
+
+        // travel through the array to find the largest element
+        for (int i = 1; i < universeOfNumbers.length; i++) {
+            // if this element is greater than the chosen one
+            if (universeOfNumbers[i] > chosenOne) {
+                chosenOne = universeOfNumbers[i]; // Update the chosen one if it is surpassed
+            }// end IF
+        }// end FOR
+
   }// end method
 }// end class
