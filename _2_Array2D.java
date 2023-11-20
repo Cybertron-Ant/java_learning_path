9311bd47ed1398b2b402ecc043d5ee42f1c19177
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-20 15:51:28 -0800

    define the 2D array using the defined dimensions, 'rows' and 'columns' variables

--- _2_Array2D.java
+++ _2_Array2D.java
@@ -5,5 +5,8 @@ class _2_Array2D {
     // create the dimensions of the 2D array, will be used later
     int rows = 3, columns = 4, value = 1; // Rows, columns, and the magician's starting value
 
+    // define the 2D array using the defined dimensions, 'rows' and 'columns' variables
+    int[][] twoDArray = new int[rows][columns];
+
     } // end method
 } // end class
