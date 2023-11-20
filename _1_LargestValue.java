8e1c2e83bb59276546509434332adcc034fe49c7
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-20 14:14:48 -0800

    create an array filled with various numbers

--- _1_LargestValue.java
+++ _1_LargestValue.java
@@ -1,5 +1,8 @@
 class _1_LargestValue {
   public static void main(String[] args) {
 
+    // create an array filled with various numbers
+    int[] universeOfNumbers = {33, 81, 107, 100, 47, 59, 58};
+
   }// end method
 }// end class
