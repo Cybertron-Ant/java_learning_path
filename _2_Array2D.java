e2878c2c231b1e28b22f86c022a2ca6d92fe062f
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-20 15:47:02 -0800

    create the dimensions of the 2D array, will be used later

--- _2_Array2D.java
+++ _2_Array2D.java
@@ -1,5 +1,9 @@
 class _2_Array2D {
     public static void main(String[] args) {
         // Add code here
+
+    // create the dimensions of the 2D array, will be used later
+    int rows = 3, columns = 4, value = 1; // Rows, columns, and the magician's starting value
+
     } // end method
 } // end class
