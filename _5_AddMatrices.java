515574b78c8b41e2c02029a523bc87ac7d992599
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-20 18:04:41 -0800

    define method 'addMatrices' to add two matrices

--- _5_AddMatrices.java
+++ _5_AddMatrices.java
@@ -6,4 +6,21 @@ class _5_AddMatrices {
       int[][] matrix2 = { { 4, 5, 9 }, { 8, 3, 6 }, { 1, 2, 7 } };
         
     }// end 'main' method
+
+    // define method 'addMatrices' to add two matrices
+    public static int[][] addMatrices(int[][] m1, int[][] m2) {
+        int rows = m1.length;
+        int columns = m1[0].length;
+        int[][] result = new int[rows][columns];// new matrix
+
+        // addition of corresponding elements in matrix 'result'
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                result[i][j] = m1[i][j] + m2[i][j];
+            }// end FOR
+        }// end FOR
+
+        return result;
+    }//end 'addMatrices' method
+
 }// end class '_5_AddMatrices'
