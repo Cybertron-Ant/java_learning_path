4484a023a55e3cb76981766a25a64193995c9dd2
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-20 16:42:12 -0800

    define the destination array where the essence will be transferred to, later

--- _3_CopyArray.java
+++ _3_CopyArray.java
@@ -4,5 +4,8 @@ class _3_CopyArray {
     // define the source array holding the elements to be transferred later
     int[] sourceEssence = {1, 2, 3, 4, 5};
 
+    // define the destination array where the essence will be transferred to, later
+    int[] destinationEssence = new int[sourceEssence.length]; // Preparing the vessel for the essence
+
   }// end method
 }// end class
