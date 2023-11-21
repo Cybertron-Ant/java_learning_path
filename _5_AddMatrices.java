626cf1d7db070ad6d68b7cc2d74125708e2c3f88
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-20 17:57:11 -0800

    define two matrices, 'matrix' and 'matrix2' variables

--- _5_AddMatrices.java
+++ _5_AddMatrices.java
@@ -1,5 +1,9 @@
 class _5_AddMatrices {
     public static void main(String[] args) {
+
+      // define two matrices, 'matrix' and 'matrix2' variables
+      int[][] matrix1 = { { 3, 7, 2 }, { 9, 2, 4 }, { 6, 1, 5 } };
+      int[][] matrix2 = { { 4, 5, 9 }, { 8, 3, 6 }, { 1, 2, 7 } };
         
     }// end 'main' method
 }// end class '_5_AddMatrices'
