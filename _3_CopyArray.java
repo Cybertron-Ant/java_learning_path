8161f42749e0dddcc6251cd335752a289ad88ed1
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-20 16:40:36 -0800

    define the source array holding the elements to be transferred later

--- _3_CopyArray.java
+++ _3_CopyArray.java
@@ -1,5 +1,8 @@
 class _3_CopyArray {
   public static void main(String[] args) {
 
+    // define the source array holding the elements to be transferred later
+    int[] sourceEssence = {1, 2, 3, 4, 5};
+
   }// end method
 }// end class
