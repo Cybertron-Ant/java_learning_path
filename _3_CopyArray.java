e0a77cba288e75ced08545d9aee5bc9a24243e5e
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-20 16:54:51 -0800

    define a method to transfer elements from one array to another

--- _3_CopyArray.java
+++ _3_CopyArray.java
@@ -16,4 +16,20 @@ class _3_CopyArray {
     System.out.println();
 
   }// end method
+
+
+  // define a method to transfer elements from one array to another
+  public static void transferEssence(int[] origin, int[] destination) {
+    // Ensure the arrays' lengths align for a successful transfer
+    if (origin.length != destination.length) {
+      System.out.println("Alas! The arrays' lengths do not align. The transfer fails.");
+      return; // Exit the ritual if the lengths mismatch
+    }
+
+    // Perform the transfer of essence from the origin to the destination array
+    for (int i = 0; i < origin.length; i++) {
+      destination[i] = origin[i]; // Channeling the elements from one array to another
+    }
+  }// end 'transferEssence' method
+  
 }// end class
