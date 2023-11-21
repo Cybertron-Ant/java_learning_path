d11c7930c1c6eef953b7ab8aed6cacc151e6acc9
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-20 16:00:08 -0800

    fill the 2D array with values using nested 'for' loops

--- _2_Array2D.java
+++ _2_Array2D.java
@@ -8,5 +8,10 @@ class _2_Array2D {
     // define the 2D array using the defined dimensions, 'rows' and 'columns' variables
     int[][] twoDArray = new int[rows][columns];
 
+    // fill the 2D array with values using nested 'for' loops
+    for (int i = 0; i < rows; i++) // journey through rows
+    for (int j = 0; j < columns; j++) // Wandering through columns
+        twoDArray[i][j] = value++; // Assigning values
+
     } // end method
 } // end class
