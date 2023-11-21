e8688439531ec80f1d16005cd1c789bb7f5438f6
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-20 17:13:11 -0800

    define two matrices to compare their equality, later

--- _4_CheckMatrixEquality.java
+++ _4_CheckMatrixEquality.java
@@ -1,5 +1,9 @@
 class _4_CheckMatrixEquality {
     public static void main(String[] args) {
+
+      // define two matrices to compare their equality, later
+      int[][] matrix1 = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+      int[][] matrix2 = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
     
     }
 }
