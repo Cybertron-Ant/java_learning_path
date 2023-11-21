a9b085f94acce018569399ef8f9d670acad4fd64
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-20 16:51:54 -0800

    see the elements the source array before the transfer begins, using a 'for' loop

--- _3_CopyArray.java
+++ _3_CopyArray.java
@@ -7,5 +7,13 @@ class _3_CopyArray {
     // define the destination array where the essence will be transferred to, later
     int[] destinationEssence = new int[sourceEssence.length]; // Preparing the vessel for the essence
 
+    
+    // see the elements the source array before the transfer begins, using a 'for' loop
+    System.out.println("Source Essence:");
+    for (int essence : sourceEssence) {
+      System.out.print(essence + " "); // displaying the essence of the source
+    }// end FOR
+    System.out.println();
+
   }// end method
 }// end class
