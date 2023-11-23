a06fb2675bf600315da9a0c8828a689383a29dc8
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-23 10:35:08 -0500

    parent class that 'TestParentClass1' will inherit from, later

---
+++ TestParentClass1.java
@@ -0,0 +1,11 @@
+package com.test;
+
+// parent class that 'TestParentClass1' will inherit from, later
+public class TestParentClass1 {
+	static int n2 = 1000;
+
+	static void multiply() {
+		System.out.println("Multiplication");
+    
+	}// end method 'multiply'
+}// end class 'TestParentClass1'
