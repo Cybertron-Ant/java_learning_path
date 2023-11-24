3ccda013ac78a8dd75a3ad93dca28284407bd20b
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-24 09:51:10 -0500

    child class, inherits from class 'Parent1

---
+++ sss.java
@@ -0,0 +1,15 @@
+package com.test;
+
+// child class, inherits from class 'Parent1' 
+public class Child1 extends Parent1{
+	static int number1 = 100;
+
+	public static void main(String[] args) {
+  
+		System.out.println(number1);
+		System.out.println(number2);
+		System.out.println(number3);
+    
+	}// end method 'main'
+
+}// end class 'Child1'
