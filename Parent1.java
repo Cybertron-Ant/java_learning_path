7c638afa2caffceb6f0b98ccb0e9042d21ea59a4
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-24 09:49:23 -0500

    parent class

---
+++ Parent1.java
@@ -0,0 +1,16 @@
+package com.test;
+
+// parent class
+public class Parent1 extends GrandParent1{
+  
+	static int number2 = 200;
+	
+	static void add() {
+    
+		Child1 child1 = new Child1();
+    
+		System.out.println(child1.number1);
+		System.out.println(number3);
+    
+	}// end method 'main'
+}// end class 'Parent1'
