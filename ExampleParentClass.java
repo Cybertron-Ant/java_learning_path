94877c1aa260d92788c721beb707e4c677e8a2f0
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-27 10:36:55 -0500

    parent class polymorphism example, eg.

--- ExampleParentClass.java
+++ ExampleParentClass.java
@@ -1,17 +1,19 @@
 package com.test;
 
-// parent class polymorphism example
+// parent class polymorphism example, eg.
 public class RunTimePolymorphismParent {
 	
+	// instance variable 'n1' initialized to 1000
 	int n1 = 1000;
 	
+	// method 'add' to perform addition and print the result
 	void add() {
 		int a = 10;
 		int b = 20;
 		int c = a + b;
     
-		System.out.println(c);
+		System.out.println(c); // print the result of addition
     
-	}// end mthod 'add'
+	}// end method 'add'
 
 }// end class 'RunTimePolymorphismParent'
