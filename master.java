47feb732740fc313e900a1f4304c72cd7e20f39c
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-27 09:56:04 -0500

    parent class 'RunTimePolymorphismParent'

--- master.java
+++ master.java
@@ -1,5 +1,15 @@
-class HelloWorld {
-  public static void main(String[] args) {
+package com.test;
 
-  }// end method
-}// end class
+// parent class 'RunTimePolymorphismParent'
+public class RunTimePolymorphismParent {
+	
+	void add() {
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+    
+		System.out.println(c);
+    
+	}// end method 'add'
+
+}// end class 'RunTimePolymorphismParent'
