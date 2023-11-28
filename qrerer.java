04c93fb55f24bf25e3c1438c0412e74f2791b9ad
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-28 10:23:36 -0500

    base class 'SingletonBaseClass' implementing the Singleton design pattern

---
+++ qrerer.java
@@ -0,0 +1,19 @@
+
+// base class 'SingletonBaseClass' implementing the Singleton design pattern
+public class SingletonBaseClass {
+	
+	// private constructor to prevent direct instantiation
+	private SingletonBaseClass(){
+		// displays a message indicating the default constructor is called
+		System.out.println("Default Constructor Called.");
+	}// end constructor 'SingletonBaseClass'
+	
+	// static instance of 'SingletonBaseClass' created upon class loading
+	static SingletonBaseClass singletonBaseClass = new SingletonBaseClass();
+	
+	// method to retrieve the 'Singleton' instance
+	static SingletonBaseClass getObject(){
+		return singletonBaseClass;
+	}// end getter method 'getObject'
+
+}// end class 'SingletonBaseClass'
