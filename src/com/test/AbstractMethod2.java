0700f0be5ab62af895f911c911b13171cf2d0c08
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-29 13:58:11 -0500

    define abstract class 'AbstractMethod2' with an abstract method and a non-abstract method

---
+++ src/com/test/AbstractMethod2.java
@@ -0,0 +1,16 @@
+package com.test;
+
+// define abstract class 'AbstractMethod2' with an abstract method and a non-abstract method
+public abstract class AbstractMethod2 {
+	
+	// abstract method 'test' without implementation
+	abstract void test();
+	
+	// non-abstract method 'multiply' with implementation
+	void multiply() {
+    
+		System.out.println("Multiplication");
+    
+	}// end method 'multiply'
+
+}// end class 'AbstractMethod2'
