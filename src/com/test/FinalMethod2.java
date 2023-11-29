594defd793733c9f3adedb2fd3a5e435aac3acbd
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-29 13:36:43 -0500

    class 'FinalMethod2' defining a method as final

---
+++ src/com/test/FinalMethod2.java
@@ -0,0 +1,12 @@
+package com.test;
+
+// class 'FinalMethod2' defining a method as final
+public class FinalMethod2 {
+	
+	// final method 'add' that cannot be overridden
+	final void add() {
+    
+		System.out.println("Addition");
+    
+	}// end method 'add'
+}// end class 'FinalMethod2'
