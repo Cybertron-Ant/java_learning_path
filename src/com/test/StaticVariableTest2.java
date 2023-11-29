aaa817f39e1ec775531d706de50521d3c06df965
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-29 11:03:42 -0500

    class 'StaticVariableTest2' showing the usage of a static variable

---
+++ src/com/test/StaticVariableTest2.java
@@ -0,0 +1,9 @@
+package com.test;
+
+// class 'StaticVariableTest2' showing the usage of a static variable
+public class StaticVariableTest2 {
+	
+	// static variable 'a' initialized to 10
+	static int a = 10;
+  
+}// end class 'StaticVariableTest2'
