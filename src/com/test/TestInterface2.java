b442cd65087cdde7ccd3c67a014c9ea354a58fea
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-30 11:27:57 -0500

    declaring a functional interface 'TestInterface2' with a single abstract method

---
+++ src/com/test/TestInterface2.java
@@ -0,0 +1,10 @@
+package com.test;
+
+// declaring a functional interface 'TestInterface2' with a single abstract method
+@FunctionalInterface
+public interface TestInterface2 {
+	
+	// abstract method 'test2' without implementation
+	void test2();
+  
+}// end interface 'TestInterface2'
