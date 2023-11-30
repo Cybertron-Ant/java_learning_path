c0e924dac7414b39027e36f4bc20f313ebdbb59f
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-30 11:29:21 -0500

    defining interface 'TestInterface1' with two abstract methods

---
+++ src/com/test/TestInterface1.java
@@ -0,0 +1,12 @@
+package com.test;
+
+// defining interface 'TestInterface1' with two abstract methods
+public interface TestInterface1 {
+	
+	// abstract method 'test' without implementation
+	void test();
+	
+	// abstract method 'test1' without implementation
+	void test1();
+  
+}// end interface 'TestInterface1'
