73bd04596ecf89c52eeb823dd3319460b988ea53
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-30 10:54:26 -0500

    an 'interface' 'MagicalCreature' defining magical properties

---
+++ src/com/test/MagicalCreature.java
@@ -0,0 +1,12 @@
+package com.test;
+
+
+// an 'interface' 'MagicalCreature' defining magical properties
+public interface MagicalCreature {
+    // define a method 'castSpell' to cast spells
+    void castSpell();
+    
+    // every creature can also display its essence
+    void displayEssence();
+  
+}// end class 'MagicalCreature'
