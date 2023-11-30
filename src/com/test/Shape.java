ff0cd229db2e12a10a66d36426532e2e6feaf1b5
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-11-30 08:18:33 -0500

    'abstract' class 'Shape', with an 'abstract' method 'calculateArea()'

---
+++ src/com/test/Shape.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/* an 'abstract' class is a class that cannot be instantiated on its own. 
+  It serves as a blueprint for other classes to inherit from. 
+  'abstract' classes can contain both abstract and non-abstract methods */
+// 'abstract' class 'Shape', with an 'abstract' method 'calculateArea()'
+abstract class Shape {
+  
+    // abstract method 'calculateArea'
+    public abstract double calculateArea();
+  
+}// end class 'Shape'
