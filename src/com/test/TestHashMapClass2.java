76c4e5bdc97599080413d9e7e09245f6c3eb0ef5
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-12-08 10:31:55 -0500

    class 'TestHashMapClass' dshows the usage of 'HashMap' by storing, retrieving, and printing key-value pairs

---
+++ src/com/test/TestHashMapClass2.java
@@ -0,0 +1,37 @@
+package com.test;
+
+import java.util.HashMap;
+
+// class 'TestHashMapClass' dshows the usage of 'HashMap' by storing, retrieving, and printing key-value pairs
+public class TestHashMapClass {
+
+	public static void main(String[] args) {
+		
+		// creating a 'HashMap' with Integer keys and String values
+		HashMap<Integer, String> testMap = new HashMap<>();
+		
+		// adding key-value pairs to the HashMap
+		testMap.put(1, "test1");
+		testMap.put(2, "test4");
+		testMap.put(1, "test3"); // Overwrites the value for key 1
+		testMap.put(4, "test4");
+		testMap.put(5, "test5");
+		
+		// printing the content of the 'HashMap'
+		System.out.println(testMap);
+		
+		// printing the value associated with key 5
+		System.out.println(testMap.get(5));
+		
+		// printing the set of keys in the 'HashMap'
+		System.out.println(testMap.keySet());
+		
+		// iterating through the 'HashMap' and printing its values
+		for(Integer key : testMap.keySet()) {
+			System.out.println(testMap.get(key));
+      
+		}// end FOR
+    
+	}// end method 'main'
+
+}// end class 'TestHashMapClass'
